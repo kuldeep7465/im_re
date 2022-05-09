19050384b21e7d32e29f75c70d0d573371e13788
kuldeep7465 <91813803+kuldeep7465@users.noreply.github.com>
2022-05-09 14:50:34 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using radio.connetion;
+using radio.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace radio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            radioEntities obj1 = new radioEntities();
+            var res = obj1.Tables.ToList();
+            return View(res);
+        }
+
+        public ActionResult form()
+        {
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult form(Class1 emp)
+        {
+            radioEntities obj1 = new radioEntities();
+            Table obj2 = new Table();
+            obj2.id = emp.id;
+            obj2.name = emp.name;
+            obj2.gender = emp.gender;
+            obj2.Date = emp.Date;
+            obj2.image = emp.image;
+           
+            obj1.Tables.Add(obj2);
+            obj1.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
